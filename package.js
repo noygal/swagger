2afe9fa4d0d51528b56cd0623d611d6bb6a91b9f
Hadar <hlandao@gmail.com>
2016-04-18 18:09:28 +0300

    updated swagger-to-typescript

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'guild:swagger',
-  version: '0.0.44',
+  version: '0.0.45',
   // Brief, one-line summary of the package.
   summary: 'Rest API based on swagger',
   // URL to the Git repository containing the source code for this package.
@@ -14,7 +14,7 @@ Npm.depends({
   'swagger-tools' : '0.10.1',
   'swagger-client': 'https://github.com/dotansimha/swagger-js/archive/087753c582727f712147118776083972f7c22e73.tar.gz',
   'js-yaml': '3.5.5',
-  'swagger-to-typescript': '1.0.31'
+  'swagger-to-typescript': '1.0.32'
 });
 
 Package.registerBuildPlugin({
@@ -26,7 +26,7 @@ Package.registerBuildPlugin({
   npmDependencies: {
     'js-yaml': '3.5.3',
     'simple-git': '1.32.1',
-    'swagger-to-typescript': '1.0.31'
+    'swagger-to-typescript': '1.0.32'
   }
 });
 
