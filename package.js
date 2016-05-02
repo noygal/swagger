d4d041a3e78183c1008753f9e115f0f9c18a13a0
dotansimha <dotansimha@gmail.com>
2016-05-02 11:55:33 +0300

    Updated deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'guild:swagger',
-  version: '0.0.55',
+  version: '0.0.56',
   // Brief, one-line summary of the package.
   summary: 'Rest API based on swagger',
   // URL to the Git repository containing the source code for this package.
@@ -25,7 +25,7 @@ Package.registerBuildPlugin({
   npmDependencies: {
     'js-yaml': '3.5.5',
     'simple-git': '1.32.1',
-    'swagger-to-typescript': '1.0.49'
+    'swagger-to-typescript': '1.0.50'
   }
 });
 
