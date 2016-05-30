87c60e9c5fd903a8981c26aed084a2e45ffc343b
Hadar Landao <hlandao@gmail.com>
2016-05-30 20:18:49 +0300

    (swagger-error): fix error message

--- swagger-server.js
+++ swagger-server.js
@@ -325,6 +325,7 @@ function getArgsFromParams(transformers, params) {
 
 class SwaggerError extends Error {
   constructor(httpCode, errorCode, errorMessage, details) {
+    super(errorMessage);
     this.httpCode = httpCode;
     this.errorCode = errorCode;
     this.errorMessage = errorMessage;
