022419e7519385402df44a8f9e9012aa0711b19f
Hadar <hlandao@gmail.com>
2016-05-31 16:35:18 +0300

    chore(build): update build files

--- build/definitions/index.d.ts
+++ build/definitions/index.d.ts
@@ -1,3 +1,8 @@
 export * from './swagger-server';
 export * from './swagger-error';
 export * from './swagger-client';
+export declare module "meteor/guild:swagger" {
+    export * from './swagger-server';
+    export * from './swagger-error';
+    export * from './swagger-client';
+}
